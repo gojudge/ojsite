0befe095ec7e13c046fadde0644882f53662673f
Laily <I@laily.net>
2016-09-08 15:57:30 +0800

    test pango

--- controllers/error.go
+++ controllers/error.go
@@ -4,10 +4,10 @@ import (
 // "github.com/astaxie/beego"
 )
 
-type ErrorController struct {
-	BaseController
-}
-
+//type ErrorController struct {
+//	BaseController
+//}
+/*
 func (this *ErrorController) Error404() {
 	this.Data["title"] = "404"
 	this.TplName = "error/404.tpl"
@@ -17,3 +17,4 @@ func (this *ErrorController) Error501() {
 	this.Data["title"] = "501"
 	this.TplName = "error/501.tpl"
 }
+*/
